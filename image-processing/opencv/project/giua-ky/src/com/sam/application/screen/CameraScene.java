9cf0db77b9b9347634557b741c31b39457d28897
tuan cat <tuan.catnghiemhieu@gmail.com>
2019-03-17 08:57:30 +0700

    - add đồ án giữa kỳ

---
+++ image-processing/opencv/project/giua-ky/src/com/sam/application/screen/CameraScene.java
@@ -0,0 +1,13 @@
+package com.sam.application.screen;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class CameraScene extends Scene{
+
+	public CameraScene(Parent root, double width, double height) {
+		super(root, width, height);
+		
+	}
+
+}
