413b08654bac1359ae8218c0cc804eb9d9e88375
tuan.cat <tuan.catnghiemhieu@gmail.com>
2019-03-31 08:57:15 +0700

    + add project cuối kỳ

---
+++ image-processing/opencv/cuoi-ky/src/com/sam/application/ui/GridMainPane.java
@@ -0,0 +1,35 @@
+package com.sam.application.ui;
+
+
+import org.opencv.core.Mat;
+
+import javafx.geometry.Insets;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+
+public class GridMainPane extends GridPane {
+	
+	private ImageView img1;
+	private ImageView img2;
+
+	public GridMainPane() {
+		// TODO Auto-generated constructor stub
+		this.setMinSize(400, 400);
+		this.setHgap(10);
+		this.setVgap(10);
+		this.setPadding(new Insets(0, 10, 0, 10));
+		initImage();
+	}
+	private void initImage() {
+		Image i = new Image(getClass().getClassLoader().getResourceAsStream("images/1.jpg"));
+		this.img1 = new ImageView(i);
+		this.img2 = new ImageView(i);
+		this.add(img1, 0, 0);
+		this.add(img2, 1, 0);
+	}
+	private void processImage() {
+		Mat src = new 
+	}
+
+}
